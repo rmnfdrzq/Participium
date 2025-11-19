748445307d7d327cbab6841d614e888280d2061c
GIULIA PRICINA s281173 <s281173@studenti.polito.it>
2025-11-19 11:35:35 +0100

    API FE refactoring

---
+++ client/src/API/functionalities/forms.mjs
@@ -0,0 +1,30 @@
+const SERVER_URL = "http://localhost:3001";
+
+// registration of municipality user given userdata {username, email, password,office_id}
+export const createMunicipalityUser = async (userData) => { // admin creates municipality user
+  const response = await fetch(`${SERVER_URL}/api/admin/createuser`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include',
+    body: JSON.stringify(userData)
+  });
+  if (!response.ok) {
+    const errDetail = await response.json();
+    throw errDetail.error;
+  }
+  return await response.json();
+};
+
+export const insertReport = async (reportData) => {
+  const response = await fetch(`${SERVER_URL}/api/reports`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include',
+    body: JSON.stringify(reportData)
+  });
+  if (!response.ok) {
+    const errDetail = await response.json();
+    throw errDetail.error;
+  }
+  return await response.json();
+};
---
+++ client/src/API/functionalities/get-all.mjs
@@ -0,0 +1,53 @@
+const SERVER_URL = "http://localhost:3001";
+
+//get only and all operators
+export const getAllOperators = async () => { //get all and only operators
+  const response = await fetch(`${SERVER_URL}/api/admin`, {
+    credentials: 'include'
+  });
+  if (!response.ok) {
+    const errDetail = await response.json();
+    throw errDetail.error;
+  }
+  return await response.json();
+};
+
+//get all default offices 
+export const getAllOffices = async () => { 
+  const response = await fetch(`${SERVER_URL}/api/offices`, {
+    credentials: "include",
+  });
+  if (response.ok) {
+    return await response.json();
+  } else {
+    const text = await response.text();
+    throw new Error(text);
+  }
+};
+
+//get all roles
+export const getAllRoles = async () => {
+  const response = await fetch(`${SERVER_URL}/api/roles`, {
+    credentials: "include", 
+  });
+  if(response.ok){
+    return await response.json();
+  } else {
+    const text = await response.text();
+    throw new Error(text);
+  }
+};
+
+// get all categories
+export const getAllCategories = async () => {
+  const response = await fetch(`${SERVER_URL}/api/categories`, {
+    credentials: "include", 
+  });
+
+  if (response.ok) {
+    return await response.json();
+  } else {
+    const text = await response.text();
+    throw new Error(text); 
+  }
+};

65bef1cbd1cf67ca177b37306ed2065c8343d8c8
Fedor Rumiantsev <theorumyantsev@gmail.com>
2025-11-25 21:45:38 +0100

    FE - fixed import of axiosinstance in API/citizen.js

--- client/src/API/citizen.js
+++ client/src/API/citizen.js
@@ -1,11 +1,11 @@
-import axiosInstance from '../axiosInstance';
+import axiosInstance from "./axiosInstance";
 
 // Get citizen profile of the logged-in user
 export const getCitizenProfile = async () => {
-  return await axiosInstance.get('/api/citizens');
-}
+  return await axiosInstance.get("/api/citizens");
+};
 
 // Update citizen profile of the logged-in user
 export const updateCitizenProfile = async (updates) => {
-  return await axiosInstance.put('/api/citizens', updates);
-}
+  return await axiosInstance.put("/api/citizens", updates);
+};
